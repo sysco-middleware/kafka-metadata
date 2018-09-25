33899de0ad848c590c5c6a320b9057f3f3826b04
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-09-25 15:51:10 +0200

    add: timeout kafka connection

--- src/main/scala/no/sysco/middleware/ktm/AppConfig.scala
+++ src/main/scala/no/sysco/middleware/ktm/AppConfig.scala
@@ -7,7 +7,7 @@ object Env extends Enumeration {
   val dev, test = Value
 }
 
-case class KafkaConfig(bootstrapServers: String)
+case class KafkaConfig(bootstrapServers: String, timeout: Long)
 case class RestConfig(serviceName: String, host: String, port: Int)
 case class ApplicationConfig(kafka: KafkaConfig, rest: RestConfig, env: Env.Value)
 
@@ -16,7 +16,7 @@ object AppConfig {
   def loadConfig(): ApplicationConfig = {
     val config = ConfigFactory.load("application.conf")
     println(config)
-    val kafka = KafkaConfig(config.getString("kafka.bootstrap-servers"))
+    val kafka = KafkaConfig(config.getString("kafka.bootstrap-servers"), config.getLong("kafka.timeout"))
     val rest = RestConfig(
       config.getString("service.name"),
       config.getString("service.host"),
